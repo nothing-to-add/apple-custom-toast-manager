0a21bd477d56db997df2f83fed5b49c86169caa8
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2025-09-09 02:55:33 +0300

    initial commit for package creation

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "apple-custom-toast-manager",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "apple-custom-toast-manager",
+            targets: ["apple-custom-toast-manager"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "apple-custom-toast-manager"),
+
+    ]
+)
